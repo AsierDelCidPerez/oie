ea4f062a0f7a1fba784851b65940ecbbe38118d0
Asier Del Cid Pérez <103944663+AsierDelCidPerez@users.noreply.github.com>
2024-01-03 15:10:35 +0100

    03/01/2024

--- competicion/quinto.cpp
+++ competicion/quinto.cpp
@@ -1,63 +1,49 @@
 #include <iostream>
+#include <queue>
 
-#include <deque>
-
-#include <vector>
-
+using ll = long long int;
 
 using namespace std;
 
+int main(){
+    int N, M, T;
 
-void bfs(int n, int m, int t) {
+    priority_queue<int, vector<int>, greater<int>> celulas;
+    vector<int> yemas;
+    priority_queue<int, vector<int>, greater<int>> aux;
 
-    deque<int> q;
-
-    q.push_back(1);
-
-
-    for (int i = 0; i < t; i++) {
-
-        int s = q.size();
-
-        while (s--) {
-
-            int j = q.front();
-
-            q.pop_front();
-
-            if (j < m) {
-
-                q.push_back(j + 1);
-
-            } else {
-
-                q.push_back(j - n + 1);
-
-                q.push_back(j + 1);
-
+    while(cin >> N >> M >> T){
+        celulas.push(N); //tiempo que tardan en reproducirse en una
+        while(T>0){
+            int min = -1;
+            aux = priority_queue<int, vector<int>, greater<int>>();
+            for(int i=0;i<celulas.size();i++){
+                int val = celulas.top(); celulas.pop();
+                min = (min == -1) ? (val) : (min);
+                min = (min > T) ? T : min;
+                T-=min;
+                if(i < yemas.size()){
+                    int &aVal = yemas[i];
+                    if(aVal < min){
+                        aux.push(N-(aVal-min));
+                        aVal = M;
+                    }else{
+                        aVal -= min;
+                        if(aVal == 0) {
+                            celulas.push(N);
+                            aVal = M;
+                        }
+                    }
+                }
+                if(val == min){
+                    celulas.push(N);
+                    yemas.push_back(M);
+                    // aux.push(M);
+                }else{
+                    celulas.push(val-min);
+                }
             }
-
         }
-
+        std::cout << celulas.size() << "\n";
     }
-
-
-    cout << q.size() << "\n";
-
-}
-
-
-int main() {
-
-    int t, n, m;
-
-    while (cin >> n >> m >> t) {
-
-        bfs(n, m, t);
-
-    }
-
-
-    return 0;
-
 }
--- exers/rutaperfecta.cpp
+++ exers/rutaperfecta.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
+#include <vector>
+#include <queue>
 
 using namespace std;
 
+int N, M;
+struct estado {
+    int nGrava, nEmp, nVege;
+};
+
+vector<vector<pair<int, pair<int, string>>>> rutas;
+
+void resolver(int const & origin, int const & dest){
+    priority_queue<pair<int, estado>, vector<pair<int, estado>>, greater<pair<int, estado>>> pq;
+    pq.push({0, {}});
+
+}
+
 int main(){
-    
+    while(cin >> N >> M){
+        rutas.assign(N, {});
+        for(int i=0;i<M;i++){
+            int a, b, c, r;
+            string tRuta;
+            cin >> a >> b >> c >> tRuta; a--;b--;
+            rutas[a].push_back({c, {b, tRuta}});
+            rutas[b].push_back({c, {a, tRuta}});
+        }
+
+        int k, a, b; cin >> k;
+
+        for(int i=0;i<k;i++){
+            cin >> a >> b;
+            resolver(a, b);
+        }
+
+    }
 }
