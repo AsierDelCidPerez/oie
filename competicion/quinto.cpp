2d84c46d43ea76f87bb215a0c803165c6a098c5b
Asier Del Cid Pérez <103944663+AsierDelCidPerez@users.noreply.github.com>
2024-01-07 18:37:44 +0100

    07/01/2023

--- competicion/quinto.cpp
+++ competicion/quinto.cpp
@@ -3,8 +3,27 @@
 
 using namespace std;
 
+int expHongos(int N, int M, int T){
+    // Principio de recursividad: 
+    int cantidad = 1;
+
+    if(M < T){
+        T-=M;
+        expHongos(N, M, T-M);
+    }
+
+    return cantidad;
+}
+
+
 int main(){
     int N, M, T;
+    /*
+        N -> tiempo que tarda una célula adulta en reproducirse
+        M -> tiempo que tarda una celula hija en convertirse en adulta
+        T -> tiempo del experimento
+        Cuestión: En tiempo T cuantas células tendremos si comenzamos con una célula adulta.
+    */
 
     priority_queue<int, vector<int>, greater<int>> celulas;
 
@@ -12,20 +31,21 @@ int main(){
         celulas = priority_queue<int, vector<int>, greater<int>>();
         celulas.push(N);
         while(T!=0){
-            int minimum = celulas.top();
+            int minimum = celulas.top(); // propiedad de priority_queue
             minimum = min(T, minimum);
             T-=minimum;
             queue<int> aux;
+
             while(!celulas.empty()){
                 int value = celulas.top(); celulas.pop();
                 if(value < minimum){
                     int dif = minimum - value; // segundos a restar después
-                    aux.push(N+M-dif);
+                    aux.push(M-dif);
                     aux.push(N-dif);
                 }else{
                     int myVal = value - minimum;
                     if(myVal == 0){
-                        aux.push(N+M);
+                        aux.push(M);
                         aux.push(N);
                     }else{
                         aux.push(myVal);
--- exers/peajeSombra.cpp
+++ exers/peajeSombra.cpp
@@ -1,13 +1,70 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <queue>
 
-
+#define MAX 1e9;
 using namespace std;
 
 vector<vector<int>> g;
 
+int N, C;
+int A, L, T;
 
+vector<int> bfsWOD(int origin){
+    vector<int> dist;
+    dist.assign(N, -1);
+    queue<int> q;
+    q.push(origin);
+    dist[origin] = 0;
+    
+    while(!q.empty()){
+        int v = q.front(); q.pop();
+        for(int ady : g[v]){
+            if(dist[ady] == -1) {
+                dist[ady] = dist[v]+1;
+                q.push(ady);
+            }
+        }
+    }
+    return dist;
+}
+
+void resolver(){
+    A--; L--; T--;
+    int origin = T;
+
+    queue<int> q;
+
+    q.push(origin);
+
+    const vector<int> dist1 = bfsWOD(A);
+    const vector<int> dist2 = bfsWOD(L);
+    const vector<int> dist3 = bfsWOD(T);
+
+    vector<int> finalista;
+    finalista.assign(N, -1);
+ 
+    for(int i=0;i<N;i++){
+        if(dist1[i] == -1 || dist2[i] == -1 || dist3[i] == -1){
+            finalista[i] = MAX; continue;
+        }
+        finalista[i] = dist1[i] + dist2[i] + dist3[i];
+    }
+    int minimum = *min_element(finalista.begin(), finalista.end());
+    cout << minimum << "\n";
+}
 
 int main() {
+    while(cin >> N >> C >> A >> L >> T){
+        g.assign(N, {});
+        for(int i=0;i<C;i++){
+            int a, b; cin >> a >> b; a--;b--;
+            g[a].push_back(b);
+            g[b].push_back(a);
+        }
 
+        resolver();
+
+    }
 }
